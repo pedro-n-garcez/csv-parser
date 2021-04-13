88a55c05c976945ff3fa8af13b9fbe6f69eca9f7
pedro-n-garcez <60759070+pedro-n-garcez@users.noreply.github.com>
2021-04-13 13:26:23 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	DataFrame df;
-	df.from_csv("C:\\Stuff\\col\\8S2021\\CPP2\\csv_parse\\csv_parse\\sample.csv");
+	df.from_csv("sample.csv");
 	df.print_all_data();
 
 	std::cout << "Student 1's email is " << df.table[0]->email << std::endl;
